15fc85a39f25c12568dfbd4038fa6f7ef40702cc
Balazs Rigo <rigo.balazs86@gmail.com>
2023-02-10 12:55:26 +0100

    Section 3 in progress

--- Bhrugen/MangoRestaurant/Mango.Services.ProductAPI/Program.cs
+++ Bhrugen/MangoRestaurant/Mango.Services.ProductAPI/Program.cs
@@ -1,3 +1,8 @@
+using AutoMapper;
+using Mango.Services.ProductAPI.DbContexts;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+
 namespace Mango.Services.ProductAPI
 {
     public class Program
@@ -7,11 +12,20 @@ namespace Mango.Services.ProductAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+           options.UseSqlServer(connectionString));
+
+            IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+            builder.Services.AddSingleton(mapper);
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            builder.Services.AddScoped<IProductRepository,ProductRepository>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen();     
 
             var app = builder.Build();
 
